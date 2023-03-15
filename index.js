5808ba9e45d246835c23b9aa766de0de22131541
Max <maxmwangikenya@gmail.com>
2023-03-15 14:58:45 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1=2;
+let num2=31;
+const multiply=num1*num2;
+console.log ("multiply")
 
-// Write your code here
+let random;
+random= Math.floor(Math.random() * 10) + 1;
+console.log("random")
+
+let num3 = 20
+let num4 = 16
+let mod = num3 % num4
+console.log(mod)
+
+const max=Math.max(1,4,20)
+
